6402a894010dcadd4cb8f43bb7ca8f91208f8367
SleepBearWolf <161960189+SleepBearWolf@users.noreply.github.com>
2024-09-06 15:41:21 +0700

    update

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -7,10 +7,18 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rb2d;     
     private Rigidbody rb3d;       
-    private bool isGrounded;
+    private bool isGrounded;      
 
     private bool is3DMode;        
     private bool facingRight = true; 
+
+    public Transform cameraTransform;   
+    public float smoothSpeed = 0.125f;  
+    public Vector3 cameraOffset;        
+
+    [SerializeField] private float minX, maxX, minY, maxY; 
+
+    private bool isPointAndClickMode = false; 
 
     void Start()
     {
@@ -21,33 +29,57 @@ public class PlayerMovement : MonoBehaviour
         if (rb3d != null)
         {
             is3DMode = true; 
-           
+            
             rb3d.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
         }
         else if (rb2d != null)
         {
-            is3DMode = false;
+            is3DMode = false; 
             
             rb2d.constraints = RigidbodyConstraints2D.FreezeRotation;
         }
-        
+        else
+        {
+            Debug.LogError("No Rigidbody On GameObject! add Rigidbody or Rigidbody2D");
+        }
     }
 
     void Update()
     {
-        if (is3DMode)
+        
+        if (Input.GetKeyDown(KeyCode.Q))
         {
+            SwitchMode();
+        }
+
+        
+        if (!isPointAndClickMode)
+        {
+            if (is3DMode)
+            {
+                
+                MovePlayer3D();
+            }
+            else
+            {
+                
+                MovePlayer2D();
+            }
+
             
-            MovePlayer3D();
+            FlipCharacter();
         }
         else
         {
             
-            MovePlayer2D();
+            HandlePointAndClickMode();
         }
+    }
 
+    void LateUpdate()
+    {
         
-        FlipCharacter();
+        CameraFollow();
     }
 
     void MovePlayer2D()
@@ -95,11 +127,33 @@ public class PlayerMovement : MonoBehaviour
 
     private void Flip()
     {
+        if (is3DMode)
+        {
+            
+            transform.Rotate(0f, 180f, 0f);
+        }
+        else
+        {
+           
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1; 
+            transform.localScale = theScale;
+        }
+    }
+
+    private void CameraFollow()
+    {
         
-        facingRight = !facingRight;
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1; 
-        transform.localScale = theScale;
+        Vector3 desiredPosition = transform.position + cameraOffset;
+
+        
+        desiredPosition.x = Mathf.Clamp(desiredPosition.x, minX, maxX);
+        desiredPosition.y = Mathf.Clamp(desiredPosition.y, minY, maxY);
+
+        
+        Vector3 smoothedPosition = Vector3.Lerp(cameraTransform.position, desiredPosition, smoothSpeed);
+        cameraTransform.position = smoothedPosition;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -131,10 +185,38 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-       
+        
         if (!is3DMode && collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = false;
+        }
+    }
+
+    
+    private void SwitchMode()
+    {
+        isPointAndClickMode = !isPointAndClickMode;  
+        if (isPointAndClickMode)
+        {
+            Debug.Log("Point & ClickMode");
+        }
+        else
+        {
+            Debug.Log("PlatformerMode");
+        }
+    }
+
+    
+    private void HandlePointAndClickMode()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider != null)
+            {
+                Debug.Log("Clicked on: " + hit.collider.name);
+                
+            }
         }
     }
 }

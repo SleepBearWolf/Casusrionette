b31067c60a9805ae9a3d3efeb897bd7668fbe602
SleepBearWolf <161960189+SleepBearWolf@users.noreply.github.com>
2024-10-17 15:00:33 +0700

    up

--- Assets/Scripts/Base/Pause Menu.cs
+++ Assets/Scripts/Base/Pause Menu.cs
@@ -1,46 +1,46 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private GameObject pauseMenuUI; 
 
-    [SerializeField] private bool isPaused;
+    [SerializeField] private bool isPaused = false; 
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            isPaused = !isPaused;
-        }
+            isPaused = !isPaused; 
 
-        if (isPaused)
-        {
-            ActivateMenu();
-        }
-        else
-        {
-            DeactiveMenu();
+            if (isPaused)
+            {
+                ActivateMenu(); 
+            }
+            else
+            {
+                DeactivateMenu(); 
+            }
         }
     }
 
     void ActivateMenu()
     {
         Time.timeScale = 0;
-        AudioListener.pause = true;
-        pauseMenuUI.SetActive(true);
+        AudioListener.pause = true; 
+        pauseMenuUI.SetActive(true); 
 
         Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
+        Cursor.lockState = CursorLockMode.None; 
     }
 
-    public void DeactiveMenu()
+    public void DeactivateMenu()
     {
-        Time.timeScale = 1;
-        AudioListener.pause = false;
-        pauseMenuUI.SetActive(false);
-        isPaused = false;
+        Time.timeScale = 1; 
+        AudioListener.pause = false; 
+        pauseMenuUI.SetActive(false); 
+        isPaused = false; 
 
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;

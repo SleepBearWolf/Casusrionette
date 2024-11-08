0abf2085237cab37602c50d0984ccef2acbba56b
SleepBearWolf <161960189+SleepBearWolf@users.noreply.github.com>
2024-11-08 17:44:40 +0700

    up

--- Assets/Scripts/Base/InteractionUI.cs
+++ Assets/Scripts/Base/InteractionUI.cs
@@ -3,65 +3,59 @@ using UnityEngine.UI;
 
 public class InteractionUI : MonoBehaviour
 {
-    public GameObject interactionUI;  // ��ҧ�ԧ��ѧ UI �����ʴ������������
-    public float interactionDistance = 2f;  // ���з������ö interact ��
-    private GameObject player;  // ��ҧ�ԧ��ѧ��Ǽ�����
-    private bool isPlayerNearby = false;  // ��Ǩ�ͺ��Ҽ�������������������
+    public GameObject interactionUI;  
+    public float interactionDistance = 2f;  
+    private GameObject player; 
+    private bool isPlayerNearby = false; 
 
     void Start()
     {
-        player = GameObject.FindWithTag("Player");  // �� player �� tag
-        interactionUI.SetActive(false);  // ��͹ UI �͹�������
+        player = GameObject.FindWithTag("Player"); 
+        interactionUI.SetActive(false);  
     }
 
     void Update()
     {
-        CheckInteractionDistance();  // ��Ǩ�ͺ������ҧ�����ҧ��������Шش interact
+        CheckInteractionDistance();  
     }
 
-    // �ѧ��ѹ��Ǩ�ͺ������ҧ
     void CheckInteractionDistance()
     {
         if (Vector2.Distance(player.transform.position, transform.position) <= interactionDistance)
         {
             if (!isPlayerNearby)
             {
-                ShowInteractionUI();  // �ʴ� UI ����ͼ������������
+                ShowInteractionUI();  
             }
             isPlayerNearby = true;
 
-            // ����ͼ����蹡����� Interact �蹻��� E
             if (Input.GetKeyDown(KeyCode.B))
             {
-                Interact();  // ���¡�ѧ��ѹ Interact
+                Interact(); 
             }
         }
         else
         {
             if (isPlayerNearby)
             {
-                HideInteractionUI();  // ��͹ UI ����ͼ������Թ�͡�
+                HideInteractionUI();
             }
             isPlayerNearby = false;
         }
     }
 
-    // �ѧ��ѹ�ʴ� UI
     void ShowInteractionUI()
     {
         interactionUI.SetActive(true);
     }
 
-    // �ѧ��ѹ��͹ UI
     void HideInteractionUI()
     {
         interactionUI.SetActive(false);
     }
 
-    // �ѧ��ѹ���ж١���¡����� Interact
     void Interact()
     {
         Debug.Log("Player is interacting...");
-        // ����������Ѻ��÷���觷���ͧ��� �� �ٴ��¡Ѻ NPC �����Դ��е�
     }
 }

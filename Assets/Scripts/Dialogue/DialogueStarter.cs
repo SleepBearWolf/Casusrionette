a1f975ac46ee1cd23f246d8ab80528f7ca974479
SleepBearWolf <161960189+SleepBearWolf@users.noreply.github.com>
2024-11-23 01:22:44 +0700

    up

---
+++ Assets/Scripts/Dialogue/DialogueStarter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using DialogueEditor; 
+
+public class DialogueStarter : MonoBehaviour
+{
+    public NPCConversation conversation; 
+
+    public void StartDialogue()
+    {
+        if (conversation != null)
+        {
+            ConversationManager.Instance.StartConversation(conversation);
+        }
+        else
+        {
+            Debug.LogWarning("Conversation not assigned in DialogueStarter!");
+        }
+    }
+}

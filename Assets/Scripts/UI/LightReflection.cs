65cfed4381d7a44c334e1988311bb86762e0bc87
SleepBearWolf <161960189+SleepBearWolf@users.noreply.github.com>
2024-11-23 16:42:39 +0700

    up

---
+++ Assets/Scripts/UI/LightReflection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightReflection : MonoBehaviour
+{
+    public GameObject lightEffect; 
+    private bool isCorrectPosition = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Table")) 
+        {
+            isCorrectPosition = true;
+            ActivateReflection();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Table"))
+        {
+            isCorrectPosition = false;
+            DeactivateReflection();
+        }
+    }
+
+    private void ActivateReflection()
+    {
+        if (lightEffect != null)
+        {
+            lightEffect.SetActive(true); 
+        }
+    }
+
+    private void DeactivateReflection()
+    {
+        if (lightEffect != null)
+        {
+            lightEffect.SetActive(false); 
+        }
+    }
+}
